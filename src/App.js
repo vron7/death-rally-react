fe4dd5bd9bb8e437f2c44aa69dff5fad5e88a34b
vron <veron.nahkur@pixster.eu>
2020-09-05 09:26:38 +0300

    'styling'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cards from './components/Cards';
 import SearchBox from './components/SearchBox';
 import {robots} from './robots'
+import './App.css'
 
 
 class App extends React.Component{
@@ -23,7 +24,7 @@ class App extends React.Component{
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearch} />
                 <Cards robots={filteredRobots} />
             </div>
