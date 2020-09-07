9e5cb82d790ea6da8a6011ffebf9e92a94ffd372
vron <veron.nahkur@pixster.eu>
2020-09-07 21:53:14 +0300

    Updates

---
+++ precache-manifest.5c608b6ca6935b9a9f4b782a7f4a250b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c0b9f0d05a72bbdefb3b570f6a9d2f",
+    "url": "/death-rally-react/index.html"
+  },
+  {
+    "revision": "5e3a30da7ff6f0d81cf0",
+    "url": "/death-rally-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4addd33fad59b5856667",
+    "url": "/death-rally-react/static/css/main.0cbdb0f0.chunk.css"
+  },
+  {
+    "revision": "5e3a30da7ff6f0d81cf0",
+    "url": "/death-rally-react/static/js/2.9d80639a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/death-rally-react/static/js/2.9d80639a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4addd33fad59b5856667",
+    "url": "/death-rally-react/static/js/main.8b4f8db8.chunk.js"
+  },
+  {
+    "revision": "cfdbc688224f54ae2f9d",
+    "url": "/death-rally-react/static/js/runtime-main.8c792567.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/death-rally-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
